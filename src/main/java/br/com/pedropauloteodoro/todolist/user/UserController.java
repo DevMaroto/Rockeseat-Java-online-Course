fe700433fe07add87d61595c5eb84e40f6946575
Pedro Teodoro <pedropauloteodoro26@gmail.com>
2023-10-12 18:42:18 -0300

    lesson three completed

--- src/main/java/br/com/pedropauloteodoro/todolist/user/UserController.java
+++ src/main/java/br/com/pedropauloteodoro/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 /**
  * Modificador
  * public = qualquer um pode acessar a classe
@@ -42,6 +44,10 @@ public class UserController {
       // Status Code ex: 200, 400, 401...
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
     }
+
+    var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(passwordHashred);
 
     var userCreated = this.userRepository.save(userModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
